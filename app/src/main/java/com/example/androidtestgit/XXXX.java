42c46bdb234ae2ff9bad7a19691ffd304a21ab13
anhdt <anhdtuet1995@gmail.com>
2022-03-17 10:35:17 +0700

    Fix bug

--- app/src/main/java/com/example/androidtestgit/XXXX.java
+++ app/src/main/java/com/example/androidtestgit/XXXX.java
@@ -5,5 +5,7 @@ import android.util.Log;
 public class XXXX {
     private static void Hihihi() {
         Log.d("anh.dt22222", "yyyadadsadyasdasdsadsa");
+        Log.d("anh.dt22222", "fix bug abc");
     }
+
 }
